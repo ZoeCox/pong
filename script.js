215ad1575bfd3ab643f203816835036beaa5c08b
ZoeCox <zoemcox16@gmail.com>
2024-08-12 08:16:43 +0100

    updates

--- script.js
+++ script.js
@@ -4,122 +4,113 @@ canvas.height = 700;
 canvas.width = 700;
 
 const paddle1 = {
-    xCoord: 20, 
-    yCoord: 290,
-    speed: 10
+  xCoord: 20,
+  yCoord: 290,
+  speed: 1,
 };
 
 const paddle2 = {
-    xCoord: 670, 
-    yCoord: 290, 
-    speed: 10
+  xCoord: 670,
+  yCoord: 290,
+  speed: 1,
 };
 
 const ball = {
-    xCoord: 300,
-    yCoord: 345, 
-    speed: 5,
-    ballReady: false
-}
+  xCoord: 300,
+  yCoord: 345,
+  speed: 5,
+  ballReady: false,
+};
 
 const dividerImage = new Image();
 dividerImage.ready = false;
 dividerImage.onload = checkReady;
-dividerImage.src = "/icons/divider.png";
+dividerImage.src = "./icons/divider.png";
 
 const paddleImage1 = new Image();
 paddleImage1.ready = false;
 paddleImage1.onload = checkReady;
-paddleImage1.src = "/icons/playerPaddle.png";
+paddleImage1.src = "./icons/playerPaddle.png";
 
 const paddleImage2 = new Image();
 paddleImage2.ready = false;
 paddleImage2.onload = checkReady;
-paddleImage2.src = "/icons/playerPaddle.png";
+paddleImage2.src = "./icons/playerPaddle.png";
 
 const keyClick = {};
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener(
+  "keydown",
+  function (event) {
     keyClick[event.key] = true;
-    playerMove(keyClick);
-}, false)
+  },
+  false
+);
 
-function playerMove (keyClick) {
-    if("ArrowUp" in keyClick) {
-        paddle1.yCoord -= paddle1.speed;
-    }
-    if("ArrowDown" in keyClick) {
-        paddle1.yCoord += paddle1.speed;
-    }
-    if("w" in keyClick) {
-        paddle2.yCoord -= paddle2.speed;
-    }
-    if("s" in keyClick) {
-        paddle2.yCoord += paddle2.speed;
-    }
-    if (paddle1.yCoord < 0) {
-        paddle1.yCoord = canvas.height - 100;}
-    if (paddle1.yCoord >= canvas.height -10) {
-        paddle1.yCoord = 0;}
-    if (paddle2.yCoord < 0) {
-        paddle2.yCoord = canvas.height - 100;}
-        if (paddle2.yCoord >= canvas.height -10) {
-            paddle2.yCoord = 0;}
-    render()
+function playerMove() {
+  if ("ArrowUp" in keyClick) {
+    paddle1.yCoord -= paddle1.speed;
+  }
+  if ("ArrowDown" in keyClick) {
+    paddle1.yCoord += paddle1.speed;
+  }
+  if ("w" in keyClick) {
+    paddle2.yCoord -= paddle2.speed;
+  }
+  if ("s" in keyClick) {
+    paddle2.yCoord += paddle2.speed;
+  }
+  if (paddle1.yCoord < 0) {
+    paddle1.yCoord = canvas.height - 100;
+  }
+  if (paddle1.yCoord >= canvas.height - 10) {
+    paddle1.yCoord = 0;
+  }
+  if (paddle2.yCoord < 0) {
+    paddle2.yCoord = canvas.height - 100;
+  }
+  if (paddle2.yCoord >= canvas.height - 10) {
+    paddle2.yCoord = 0;
+  }
 }
 
 document.addEventListener(
-    "keyup",
-    function (event) {
-      delete keyClick[event.key];
-    },
-    false
-  );
+  "keyup",
+  function (event) {
+    delete keyClick[event.key];
+  },
+  false
+);
 
 let randomNumber = Math.round(Math.random() * 10);
 
-
-// function ballMove() {
-//     if (randomNumber <= 2) {
-//      ball.xCoord = randomNumber + 10;  
-//     randomNumber = Math.round(Math.random() * 10);}  
-//     if (randomNumber <= 4 && randomNumber > 2) {
-//         ball.xCoord = randomNumber + 10;  
-//        randomNumber = Math.round(Math.random() * 10);}   
-//        if (randomNumber <= 6 && randomNumber > 4) {
-//         ball.xCoord = randomNumber + 10;  
-//        randomNumber = Math.round(Math.random() * 10);}   
-//        if (randomNumber <= 8 && randomNumber > 6) {
-//         ball.xCoord = randomNumber + 10;  
-//        randomNumber = Math.round(Math.random() * 10);}    
-// }
-
 function checkReady() {
-    this.ready = true;
-    playGame();
-  }
-  
-  function playGame() {
-    render();
-    requestAnimationFrame(playGame);
-  }
+  this.ready = true;
+  playGame();
+}
 
-function render() {  
-    context.fillStyle = "#b7c9e2";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+function playGame() {
+  render();
+  requestAnimationFrame(playGame);
+}
 
-    context.drawImage(dividerImage, 335, -25, 10, 750);
-    context.drawImage(paddleImage1, paddle1.xCoord, paddle1.yCoord, 10, 100);
-    context.drawImage(paddleImage2, paddle2.xCoord, paddle2.yCoord, 10, 100);
+function render() {
+  playerMove();
+  context.fillStyle = "#b7c9e2";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
-    context.beginPath();
-    context.arc(ball.xCoord, ball.yCoord, 10, 0, Math.PI * 2, true);
-    context.fillStyle = "white";
-    context.lineWidth = 2.5;
-    context.stroke();
-    context.fill();  
+  context.drawImage(dividerImage, 335, -25, 10, 750);
+  context.drawImage(paddleImage1, paddle1.xCoord, paddle1.yCoord, 10, 100);
+  context.drawImage(paddleImage2, paddle2.xCoord, paddle2.yCoord, 10, 100);
 
-    ballMove();
-};
+  context.beginPath();
+  context.arc(ball.xCoord, ball.yCoord, 10, 0, Math.PI * 2, true);
+  context.fillStyle = "white";
+  context.lineWidth = 2.5;
+  context.stroke();
+  context.fill();
+
+  //   ballMove();
+}
 
 document.body.appendChild(canvas);
