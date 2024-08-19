d28607b1cd6c00de6d555a354862f758d261531d
ZoeCox <zoemcox16@gmail.com>
2024-08-19 07:52:28 +0100

    styling

--- dropdown.js
+++ dropdown.js
@@ -1,20 +1,25 @@
 const dropDown = document.querySelector(".drop-icon");
+const hideDropDown = document.querySelector(".hide-icon");
 const instruction = document.querySelector(".instruction");
 instruction.style.display = "none";
+hideDropDown.style.display = "none";
 dropDown.style.transform = "rotate(180deg)";
+hideDropDown.style.transform = "rotate(360deg)";
 let dropDownExpanded = false;
 
 function openDropdown() {
   {
     instruction.style.display = "flex";
-    dropDown.style.transform = "rotate(180deg)";
+    dropDown.style.display = "none";
+    hideDropDown.style.display = "flex";
     dropDownExpanded = true;
   }
 }
 
 function closeDropdown() {
   instruction.style.display = "none";
-  dropDown.style.transform = "rotate(180deg)";
+  hideDropDown.style.display = "none";
+  dropDown.style.display = "flex";
   dropDownExpanded = false;
 }
 
@@ -24,4 +29,10 @@ dropDown.addEventListener("click", function () {
   }
   //  else if (dropDownExpanded);
   // closeDropdown();
+});
+
+hideDropDown.addEventListener("click", function () {
+  if (dropDownExpanded) {
+    closeDropdown();
+  }
 });
